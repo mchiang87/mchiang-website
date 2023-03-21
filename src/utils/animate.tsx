146dd0513b2f9c625bb344a184bb9bbd8da95700
vaelan <mchiang87@gmail.com>
2023-03-21 11:16:16 -0500

    minor tweaks

--- src/utils/animate.tsx
+++ src/utils/animate.tsx
@@ -5,16 +5,11 @@ export const animate = () => {
 
   const callback = (entry) => {
     console.log(entry);
-    // Get intersection data here
-    // Like entry.intersectionRatio
-
-    // Here we can set state or update ref
-    // based on entry data
   };
 
   const observer = new IntersectionObserver(callback, {
     root: ref.current,
-    // Creates a threshold of with increments of 0.01
+
     threshold: new Array(101).fill(0).map((v, i) => i * 0.01),
   });
 
